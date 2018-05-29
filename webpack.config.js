2ea15e73281e70e803581644dee60edc3e0d6c1d
Nazrul Muhaimin <nazrul.muhaimin@gmail.com>
2018-05-30 07:24:01 +0800

    update setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,57 @@ const path = require('path');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const extractSass = new ExtractTextPlugin({
+  filename: 'public/app.css'
+})
+
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack/hot/only-dev-server',
-    './client/app.jsx'
+    './resources/assets/sass/app.scss',
+    './resources/assets/scripts/app.js',
+    'tether',
   ],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    // path: path.resolve(__dirname, 'build'),
+    // filename: 'bundle.js'
+    filename: 'public/app.js'
   },
-  devServer: {
-    contentBase: 'build/', // Relative directory for base of server
-    publicPath: '/', // Live-reload
-    inline: true,
-    port: process.env.PORT || 3000, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
-    historyApiFallback: true,
-  },
+  // devServer: {
+  //   contentBase: '/app', // Relative directory for base of server
+  //   publicPath: '/public', // Live-reload
+  //   inline: true,
+  //   port: process.env.PORT || 3000, // Port Number
+  //   host: 'localhost', // Change to '0.0.0.0' for external facing server
+  //   historyApiFallback: true,
+  // },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('main.css'),
+    //new ExtractTextPlugin('main.css'),
+    extractSass,
     new TransferWebpackPlugin([
       { from: 'client' },
     ])
   ],
   module: {
     rules: [
+      // {
+      //   test: /\.jsx?$/,
+      //   exclude: /node_modules/,
+      //   loader: 'babel-loader',
+      //   query: {
+      //     cacheDirectory: true,
+      //   },
+      // },
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
+        test: /\.js$/,
+        exclude: [/node_modules/],
         loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-        },
+        options: { presets: ['env'] }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: extractSass.extract(['css-loader', 'sass-loader'])
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
